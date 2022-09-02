b5da147a3cff5253d9966e140450af140f443944
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-09-02 14:14:58 -0300

    chore: setup http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.post('/interactions', (req, res) => {
+    console.log('got interaction');
+})
+
+app.listen(PORT, () => {
+    console.log('Server started on port', PORT);
+});
