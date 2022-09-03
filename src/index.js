d9223ef1750fc3cb0ce4f882396ec72d09cf9466
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-09-03 01:14:50 -0300

    feat: discord bot

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { REST } from '@discordjs/rest';
 const token = process.env.DISCORD_TOKEN;
 const clientId = process.env.APP_ID;
 const guildId = process.env.GUILD_ID;
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.DirectMessages] });
 
 const commands = [
     new SlashCommandBuilder().setName('play').setDescription('Play your favorite songs!'),
@@ -18,24 +18,11 @@ rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
     .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
     .catch(console.error);
 
-client.on('ready', () => {
-    console.log(client.user.tag)
-});
+client.on('ready', () => console.log(`Bot ${client.user.tag} is ready`));
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const { commandName } = interaction;
-
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
-    if (commandName === 'play') {
-        console.log(interaction);
-        await interaction.reply('\'Sup?')
-    }
-
-    console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+client.on('messageCreate', async (message) => {
+    console.log("DEBUG");
+    console.log('content: ' + message.content);
 });
 
 client.login(token);
