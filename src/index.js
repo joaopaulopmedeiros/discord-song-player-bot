88ea3078ec0f6d004317b50713e94ceedf79ad2a
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-09-02 23:01:45 -0300

    feat: / endpoint

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', function (req, res) {
+    return res.json({ message: 'hello world' });
+})
+
 app.post('/interactions', function (req, res) {
     const { type } = req.body;
 
