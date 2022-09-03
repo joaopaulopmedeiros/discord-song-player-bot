ab081897c6c894a7d8d1d94f91d1439e322dc43b
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-09-03 01:54:43 -0300

    style: use single quote

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,11 +1,11 @@
 const { i18n } = require('../utils/i18n.js');
 
 module.exports = {
-    name: "play",
+    name: 'play',
     cooldown: 3,
-    aliases: ["p"],
-    description: i18n.__("play.description"),
-    permissions: ["CONNECT", "SPEAK", "ADD_REACTIONS", "MANAGE_MESSAGES"],
+    aliases: ['p'],
+    description: i18n.__('play.description'),
+    permissions: ['CONNECT', 'SPEAK', 'ADD_REACTIONS', 'MANAGE_MESSAGES'],
     async execute(interaction) {
         return interaction.reply('What\'s up?');
     },
